9427f459af44f4ca4f39d5c2a177ae5ed489091b
Fercain <117948718+maxlepesij@users.noreply.github.com>
2023-01-26 22:03:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+const menulink = document.querySelectorAll('.menu__link');
+if (iconMenu){
+    iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
+let menuArrows = document.querySelectorAll('.menu-room__item');
+if (menuArrows){
+    if (menuArrows.length > 0){
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function(e) {
+                menuArrow.classList.toggle('_active');
+            });
+        }
+    }
+}
+
+
+new Swiper('.image-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoHeight: true,
+    loop: true,
+    speed: 600,
+});
